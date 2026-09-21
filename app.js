15e6365b44b2817530bedcfbd3d237992e67a884
anca-ro <a.rosenboom6@gmail.com>
2026-09-21 20:26:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,12 +11,7 @@ function berlinDateParts() {
 }
 
 function getAdventState() {
-  const d = berlinDateParts();
-  if (d.month < 12 || (d.month === 12 && d.day < 1))
-    return { state: "before", day: 0, year: d.year };
-  if (d.month > 12 || (d.month === 12 && d.day > END_DAY))
-    return { state: "after", day: END_DAY, year: d.year };
-  return { state: "during", day: d.day, year: d.year };
+  return { state: "during", day: 10, year: 2026 };
 }
 
 function render() {
